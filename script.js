d70a43deb91993e95524a3a96c7d70033c112140
Olamide101-3mt <wahabolamide466@gmail.com>
2025-07-04 21:08:41 +0100

    Formatting the login and signup page

---
+++ script.js
@@ -0,0 +1,55 @@
+//Login.js
+const form = document.getElementById('login-form');
+const submit = document.getElementById('login-button');
+
+console.log(submit);
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  submit.disabled = true;
+
+
+// To Gather input values
+const identifier = form.identifier.value.trim();
+const password = form.password.value.trim();
+
+//Build the request
+try{
+  const res = await fetch ('/api/v1/moodbot/auth/login', {
+    method : 'POST',
+    headers : {'Content-Type': 'application/json'},
+    body : JSON.stringify({ identifier, password})
+  });
+
+  //Handle each Status code
+  if (res.status === 200) {
+    const { accessToken, refreshToken} = await res.json();
+
+    sessionStorage.setItem('mb_access', accessToken);
+    sessionStorage.setItem('mb_refresh', refreshToken);
+
+    //Redirect to a proctected page
+    window.location.href = 'chat.html';
+
+  }
+  else if (res.status === 401) {
+    const {message } = await res.json();
+    alert(message || 'Please verify your e-mail before logging in.');
+  }
+  else {
+    //400 or any other error
+    const { message } = await res.json();
+    alert(message || 'Invalid credentials. Please try again.');
+  }
+}
+catch (err) {
+  console.error(err);
+  alert('Network error. Check your connection and try again.');
+}
+finally {
+  submit.disabled = false;
+}
+
+})
+
+console.log(submit);
---
+++ signup.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('signup-form');
+const submit = form.querySelector('.login-button');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  submit.disabled = true;
+
+  const rawId = form.identifier.value.trim();
+  const password = form.password.value.trim();
+
+  //Decide if the user typed an email an email or a username
+  const looksLikeEmail = rawId.includes('@');
+
+  //Build Payload to satisfy backendschema
+  const payload = {
+    username :  looksLikeEmail ? undefined : rawId,
+    email : looksLikeEmail ? rawId : undefined,
+    password
+  };
+
+  try {
+    const res = await fetch ('/api/v1/moodbot/auth/register', {
+      method : 'POST',
+      headers: {'Content-Type': 'application/json' },
+      body : JSON.stringify(payload) 
+    });
+
+    if (res.ok) {
+      alert('Registration successful! Please verify your email, then log in.');
+      window.location.href = 'chat.html';
+    }else {
+      const { message } = await res.json().catch(() => ({}));
+      alert(message || 'Sign-up failed. Please try again.');
+    }
+  }catch (err) {
+    console.error(err);
+    alert('Network error. Please try again later.');
+  }finally {
+    submit.disabled = false;
+  }
+});
